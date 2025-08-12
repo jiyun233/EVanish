f5269922cb12141ecb1ea2b67e5cc227effac65a
jiyun233 <jjiyun114514@163.com>
2025-08-12 13:33:19 +0800

    fix: remove react depend

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -2,7 +2,6 @@
 import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { nextTick, provide } from "vue";
-import { Analytics } from "@vercel/analytics/next"
 import HomePage from "../components/HomePage.vue";
 
 const { isDark } = useData();
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from "vue";
 import type { Theme } from "vitepress";
+import { inject } from "@vercel/analytics";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 // import './custom.css'
@@ -9,7 +10,10 @@ import Layout from "./Layout.vue";
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(Layout);
-  },
+  Layout: () => h(Layout),
+  enhanceApp({ router }) {
+    if (typeof window !== "undefined") {
+      inject();
+    }
+  }
 } satisfies Theme;
